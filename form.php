3240c7a8a0873d47ff3e0a16b1ebd7346695e483
Christian Nüssli <63254403+chnuessli@users.noreply.github.com>
2020-11-01 15:48:54 +0100

    Insert contact form

---
+++ form.php
@@ -0,0 +1,47 @@
+<?php
+$errors = '';
+$myemail = 'chrigi@chnuessli.ch';//<-----Put Your email address here.
+if(empty($_POST['name'])  || 
+   empty($_POST['email']) || 
+   empty($_POST['message']))
+{
+    $errors .= "\n Bitte fülle alle Pflichtfelder aus.";
+}
+
+$name = $_POST['name']; 
+$email_address = $_POST['email']; 
+$message = $_POST['message']; 
+
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
+
+if( empty($errors))
+
+{
+
+$to = $myemail;
+
+$email_subject = "Neue Meldung auf Defikarte.ch von: $name";
+
+$email_body = "Hier ist ein neuer Defibrillator um in die Karte aufgenommen zu werden: ".
+
+" Here are the details:\n Name: $name \n ".
+
+"Email: $email_address\n Message \n $message";
+
+$headers = "From: $myemail\n";
+
+$headers .= "Reply-To: $email_address";
+
+mail($to,$email_subject,$email_body,$headers);
+
+//redirect to the 'report_success' page
+
+header('Location: report_success.html');
+
+}
+?>
