f8994cd5a0614d07b685ea468d7012cd96a1cbf9
Christian Nüssli <63254403+chnuessli@users.noreply.github.com>
2020-11-10 21:21:41 +0100

    Update form.php

--- mail/form.php
+++ mail/form.php
@@ -1,19 +1,18 @@
 <?php
+    $reporter = strip_tags(htmlspecialchars($_POST['reporter'])); 
+    $email_address = strip_tags(htmlspecialchars($_POST['email'])); 
+    $place = strip_tags(htmlspecialchars($_POST['place'])); 
+    $defi_location = strip_tags(htmlspecialchars($_POST['defi_location']));
+    $access = strip_tags(htmlspecialchars($_POST['access'])); 
+    $indoor = strip_tags(htmlspecialchars($_POST['indoor'])); 
+    $opening_hours = strip_tags(htmlspecialchars($_POST['opening_hours'])); 
+    $description = strip_tags(htmlspecialchars($_POST['description'])); 
+    $operator = strip_tags(htmlspecialchars($_POST['operator'])); 
+    $operator_phone = strip_tags(htmlspecialchars($_POST['operator_phone']));
 
-$reporter = $_POST['reporter']; 
-$email_address = $_POST['email']; 
-$place = $_POST['place'];
-$defi_location = $_POST['defi_location'];
-$access = $_POST['access'];
-$indoor = $_POST['indoor'];
-$opening_hours = $_POST['opening_hours'];
-$description = $_POST['description'];      
-$operator = $_POST['operator'];
-$operator_phone = $_POST['operator_phone'];  
-
-if (!preg_match(
-"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
-$email_address))
+    if (!preg_match(
+    "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+    $email_address));
 {
     $errors .= "\n Error: Invalid email address";
 }
@@ -21,36 +20,35 @@ $email_address))
 if( empty($errors))
 
 {
+    $to = 'chrigi@chnuessli.ch';
 
-$to = 'chrigi@chnuessli.ch';
-$from= 'info@defikarte.ch';
+    $email_subject = "Neue Meldung auf Defikarte.ch von: $reporter";
 
-$email_subject = "Neue Meldung auf Defikarte.ch von: $reporter";
 
-$email_body = "Hier ist ein neuer Defibrillator um in die Karte aufgenommen zu werden: ".
+    $email_body = "Hier ist ein neuer Defibrillator um in die Karte aufgenommen zu werden:\n";
+    "Name: $reporter\n
+    Email: $email_address\n
+    Ort: $place\n
+    Defibrillator Standort: $defi_location\n
+    Zugang: $access\n
+    Im Gebäude: $indoor\n
+    Öffnungszeiten: $opening_hours\n
+    Beschreibung: $description\n
+    Betreiber: $operator\n
+    Telefon Betreiber: $operator_phone\n";
 
-"Name: $reporter \n
-Email: $email_address\n
-Ort: $place\n
-Defibrillator Standort: $defi_location\n
-Zugang: $access\n
-Im Gebäude: $indoor\n
-Öffnungszeiten: $opening_hours\n
-Beschreibung: $description\n
-Betreiber: $operator\n
-Telefon Betreiber: $operator_phone\n";
+    $headers = "From: info@defikarte.ch\n";
 
-$headers = "From: info@defikarte.ch\n";
+    $headers .= "Reply-To: $email_address";
 
-$headers .= "Reply-To: $email_address";
-
-mail($to,$email_subject,$email_body,$headers);
+    mail($to,$email_subject,$email_body,$headers);
 
 //redirect to the 'report_success' page
 
-header('Location: report_success.html');
+    header('Location: ../report_success.html');
 
 }
+
 $errors = '';
 $myemail = 'chrigi@chnuessli.ch';//<-----Put Your email address here.
 if(empty($_POST['reporter'])  || 
