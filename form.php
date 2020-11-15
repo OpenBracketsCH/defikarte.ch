e3d865d7b8a94ab9b5353de44c678458854fd5c2
Christian Nüssli <63254403+chnuessli@users.noreply.github.com>
2020-11-15 12:27:49 +0100

    updated website with form

--- form.php
+++ form.php
@@ -1,4 +1,5 @@
-<?php charset='utf8';
+<?php 
+   header("Content-Type: text/html; charset=utf-8");
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
@@ -22,22 +23,22 @@ if(empty($_POST['name'])  		||
    $operator_phone = strip_tags(htmlspecialchars($_POST['operator_phone']));
 	
 // Create the email and send the message
-$to = 'chrigi@chnuessli.ch'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$subject = "Neuer Defibrillator";
-$email_body = "Hier ist ein neuer Defibrillator um in die Karte aufgenommen zu werden:\n
-Name: $name\n
-Email: $email_address\n
-Ort: $place\n
-Defibrillator Standort: $defi_location\n
-Zugang: $access\n
-Im Gebäude: $indoor\n
-Oeffnungszeiten: $opening_hours\n
-Beschreibung: $description\n
-Betreiber: $operator\n
-Telefon Betreiber: $operator_phone\n";
-$headers = "From: info@defikarte.ch"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-//$headers2 .= "Reply-To: $email_address";	
-mail($to,$subject,$email_body,$headers);
-header('Location: success.html');
-return true;
+   $to = 'chrigi@chnuessli.ch'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+   $subject = "Neuer Defibrillator";
+   $email_body = "Hier ist ein neuer Defibrillator um in die Karte aufgenommen zu werden:\n
+   Name: $name\n
+   Email: $email_address\n
+   Ort: $place\n
+   Defibrillator Standort: $defi_location\n
+   Zugang: $access\n
+   Im Gebäude: $indoor\n
+   Öffnungszeiten: $opening_hours\n
+   Beschreibung: $description\n
+   Betreiber: $operator\n
+   Telefon Betreiber: $operator_phone\n";
+   $headers = "From: info@defikarte.ch"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+   //$headers2 .= "Reply-To: $email_address";	
+   mail($to,$subject,$email_body,$headers);
+   header('Location: success.html');
+   return true;
 ?>
