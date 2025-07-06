701f262de3f5cab10c55583f4e74909cc6e11271
MUEHLHEIM Marc <marc.muehlheim@hexagon.com>
2025-07-06 14:01:35 +0200

    Make logo on splashscreen clear

--- web/src/components/ui/splash-screen/SplashScreen.tsx
+++ web/src/components/ui/splash-screen/SplashScreen.tsx
@@ -2,8 +2,11 @@ import logoGreen from '../../../assets/logo/defikarte-logo-quer-gruen-positiv-rg
 
 export const SplashScreen = () => {
   return (
-    <div className="z-20 absolute top-0 bg-white h-full w-full flex justify-center items-center animate-pulse-slow">
-      <img src={logoGreen} alt="defikarte.ch" className="w-50 " />
-    </div>
+    <>
+      <div className="z-20 absolute top-0 bg-white h-full w-full animate-pulse-slow"></div>
+      <div className="z-20 absolute top-0 h-full w-full flex justify-center items-center">
+        <img src={logoGreen} alt="defikarte.ch" className="w-50 animate-bounce" />
+      </div>
+    </>
   );
 };
