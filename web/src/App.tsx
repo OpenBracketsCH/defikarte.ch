c7434e6d2c3ec4f4e117238a8ea50c431b82a21d
MUEHLHEIM Marc <marc.muehlheim@hexagon.com>
2025-09-13 22:23:56 +0200

    Add lazy loading for static pages

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React, { lazy, useEffect, useState } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { useMediaQuery } from 'react-responsive';
-import { Route, Routes } from 'react-router';
+import { Route, Routes, useLocation } from 'react-router';
+import { SuspenseWrapper } from './components/ui/suspense-wrapper/SupsenseWrapper';
 import { Home } from './modules/home/Home';
-import { Knowledge } from './modules/knowledge/Knowledge';
 import { Navbar } from './modules/nav-bar/NavBar';
-import { Privacy } from './modules/privacy/Privacy';
-import { Project } from './modules/project/Project';
-import { Sponsors } from './modules/sponsors/Sponsors';
+
+const Knowledge = lazy(() =>
+  import('./modules/knowledge/Knowledge').then(module => ({ default: module.Knowledge }))
+);
+const Privacy = lazy(() =>
+  import('./modules/privacy/Privacy').then(module => ({ default: module.Privacy }))
+);
+const Project = lazy(() =>
+  import('./modules/project/Project').then(module => ({ default: module.Project }))
+);
+const Sponsors = lazy(() =>
+  import('./modules/sponsors/Sponsors').then(module => ({ default: module.Sponsors }))
+);
+
+const ScrollToTop = () => {
+  // Extracts pathname property(key) from an object
+  const { pathname } = useLocation();
+
+  // Automatically scrolls to top whenever pathname changes
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 const App: React.FC = () => {
   const isMobile = useMediaQuery({ maxWidth: 768 });
@@ -37,12 +59,41 @@ const App: React.FC = () => {
         toastOptions={{ duration: 7200 }}
       />
       <Navbar />
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/knowledge" element={<Knowledge />} />
-        <Route path="/project" element={<Project />} />
-        <Route path="/sponsors" element={<Sponsors />} />
-        <Route path="/privacy" element={<Privacy />} />
+        <Route
+          path="/knowledge"
+          element={
+            <SuspenseWrapper>
+              <Knowledge />
+            </SuspenseWrapper>
+          }
+        />
+        <Route
+          path="/project"
+          element={
+            <SuspenseWrapper>
+              <Project />
+            </SuspenseWrapper>
+          }
+        />
+        <Route
+          path="/sponsors"
+          element={
+            <SuspenseWrapper>
+              <Sponsors />
+            </SuspenseWrapper>
+          }
+        />
+        <Route
+          path="/privacy"
+          element={
+            <SuspenseWrapper>
+              <Privacy />
+            </SuspenseWrapper>
+          }
+        />
       </Routes>
     </div>
   );
