ef9880292bb802be91390ca9a53283260c7d39fc
MUEHLHEIM Marc <marc.muehlheim@hexagon.com>
2025-09-22 22:54:10 +0200

    Replace image in splash screen

--- web/src/components/ui/splash-screen/SplashScreen.tsx
+++ web/src/components/ui/splash-screen/SplashScreen.tsx
@@ -1,11 +1,11 @@
-import logoGreen from '../../../assets/logo/defikarte-logo-quer-gruen-positiv-rgb.svg';
+import iconDefikarte from '../../../assets/icons/icon-defikarte.svg';
 
 export const SplashScreen = () => {
   return (
     <>
       <div className="z-20 absolute top-0 bg-white h-full w-full animate-pulse-slow"></div>
       <div className="z-20 absolute top-0 h-full w-full flex justify-center items-center">
-        <img src={logoGreen} alt="defikarte.ch" className="w-50 animate-bounce" />
+        <img src={iconDefikarte} alt="defikarte.ch" className="w-15 animate-bounce" />
       </div>
     </>
   );
