5d8ebab1112001f84db8a4dd8c3eada4a6b3b09b
MUEHLHEIM Marc <marc.muehlheim@hexagon.com>
2025-09-23 07:41:25 +0200

    Add max heigth to md-form

--- web/src/modules/map/controls/create-aed-control/aed-form/AedForm.tsx
+++ web/src/modules/map/controls/create-aed-control/aed-form/AedForm.tsx
@@ -85,7 +85,7 @@ export const AedForm = ({ map, form, setCreateMode, onSuccess }: AedFormProps) =
   const latitude = watch('latitude');
   const title = watch('id') ? t('editAed') : t('createAed');
   return (
-    <div className="absolute z-10 h-auto lg:w-[555px] rounded-2xl bottom-16 md:bottom-22 top-4 right-4 left-4 lg:bottom-6 md:top-6 md:right-6 md:left-6 lg:left-auto bg-primary-100-white shadow-custom-lg shadow-green-shadow-64">
+    <div className="absolute z-10 h-auto lg:w-[555px] md:max-h-[600px] rounded-2xl bottom-16 md:bottom-22 top-4 right-4 left-4 lg:bottom-6 md:top-6 md:right-6 md:left-6 lg:left-auto bg-primary-100-white shadow-custom-lg shadow-green-shadow-64">
       <form className="flex flex-col h-[100%]" onSubmit={handleSubmit(onSubmit)}>
         <h1 className="border-b p-4 border-primary-05-green-05">{title}</h1>
         <div className="flex flex-col justify-start px-4 pt-5 pb-4 gap-4 overflow-auto">
