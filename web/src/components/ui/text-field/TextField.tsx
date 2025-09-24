7199cb871148b7b6fb5457f98f13577e5691b827
MUEHLHEIM Marc <marc.muehlheim@hexagon.com>
2025-09-24 19:17:47 +0200

    Try to fix zoom on form

--- web/src/components/ui/text-field/TextField.tsx
+++ web/src/components/ui/text-field/TextField.tsx
@@ -1,3 +1,4 @@
+import cn from 'classnames';
 import { HTMLInputTypeAttribute, useId } from 'react';
 import { useTranslation } from 'react-i18next';
 import { TooltipData } from '../../../model/app';
@@ -10,10 +11,15 @@ interface TextFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
   error?: string;
 }
 
+const isMobileAgent = () => {
+  return /Mobi|Android/i.test(navigator.userAgent);
+};
+
 export const TextField = ({ label, type, tooltip, error, ...props }: TextFieldProps) => {
   const { t } = useTranslation();
   const id = useId();
 
+  const isMobile = isMobileAgent();
   return (
     <div className="flex flex-col gap-1.5 text-xs leading-[150%]">
       <div className="flex flex-row gap-0.5 align-middle">
@@ -26,7 +32,12 @@ export const TextField = ({ label, type, tooltip, error, ...props }: TextFieldPr
         formNoValidate
         id={id}
         type={type}
-        className="px-3 py-2.5 align-middle rounded-lg font-normal text-primary-100-black placeholder:text-primary-60-green-04 border border-primary-10-green-05 focus:outline focus:border-primary-10-green-05 focus:outline-primary-10-green-05 bg-primary-100-white peer"
+        className={cn(
+          {
+            'text-base': isMobile,
+          },
+          'px-3 py-2.5 align-middle rounded-lg font-normal text-primary-100-black placeholder:text-primary-60-green-04 border border-primary-10-green-05 focus:outline focus:border-primary-10-green-05 focus:outline-primary-10-green-05 bg-primary-100-white peer'
+        )}
         {...props}
       />
       {!error && (
