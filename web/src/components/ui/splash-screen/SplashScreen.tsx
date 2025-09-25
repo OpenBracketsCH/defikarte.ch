20b2599e6d26b5315c43bd4b58d2bb70af7e1583
MUEHLHEIM Marc <marc.muehlheim@hexagon.com>
2025-09-25 09:29:09 +0200

    Remove animation from splash-screen background

--- web/src/components/ui/splash-screen/SplashScreen.tsx
+++ web/src/components/ui/splash-screen/SplashScreen.tsx
@@ -3,7 +3,7 @@ import iconDefikarte from '../../../assets/icons/icon-defikarte.svg';
 export const SplashScreen = () => {
   return (
     <>
-      <div className="z-20 absolute top-0 bg-white h-full w-full animate-pulse-slow"></div>
+      <div className="z-20 absolute top-0 bg-primary-60-white h-full w-full"></div>
       <div className="z-20 absolute top-0 h-full w-full flex justify-center items-center">
         <img src={iconDefikarte} alt="defikarte.ch" className="w-15 animate-bounce" />
       </div>
