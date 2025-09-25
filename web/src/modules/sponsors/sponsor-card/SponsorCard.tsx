b7797d4ebfca1f3793028b60b38e063a4a146151
MUEHLHEIM Marc <marc.muehlheim@hexagon.com>
2025-09-25 07:55:12 +0200

    Make gold and silver sponsors full width on mobile

--- web/src/modules/sponsors/sponsor-card/SponsorCard.tsx
+++ web/src/modules/sponsors/sponsor-card/SponsorCard.tsx
@@ -5,9 +5,9 @@ type SponsorCardProps = {
 
 export const SponsorCard = ({ src, href }: SponsorCardProps) => {
   return (
-    <a href={href} className="cursor-pointer" target="_blank">
-      <div className="w-77 h-50 rounded-2xl flex justify-center items-center bg-primary-100-white">
-        <img src={src} className="max-w-58" />
+    <a href={href} className="cursor-pointer w-full md:w-auto" target="_blank">
+      <div className="w-full px-8 md:w-77 h-50 rounded-2xl flex justify-center items-center bg-primary-100-white">
+        <img src={src} className="md:max-w-58" />
       </div>
     </a>
   );
